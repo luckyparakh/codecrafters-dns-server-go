1afca35a63159ce20ecb80825373beefa43ab990
luckyparakh <luckyparakh@gmail.com>
2026-02-02 16:51:08 +0530

    Add DNS message structure and enable debug logging

---
+++ app/header.go
@@ -0,0 +1,6 @@
+package main
+
+type Header struct {
+	ID uint16 // 16 bit
+	// QR 
+}
--- app/main.go
+++ app/main.go
@@ -1,9 +1,15 @@
+// https://github.com/EmilHernvall/dnsguide/blob/b52da3b32b27c81e5c6729ac14fe01fef8b1b593/chapter1.md
+// https://en.wikipedia.org/wiki/Domain_Name_System#DNS_message_format
 package main
 
 import (
 	"fmt"
 	"net"
 )
+
+type Message struct {
+	Header
+}
 
 func main() {
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
@@ -28,8 +34,9 @@ func main() {
 			break
 		}
 
-		receivedData := string(buf[:size])
-		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+		receivedData := buf[:size]
+		receivedDatas := string(buf[:size])
+		fmt.Printf("Received %d bytes from %s: %b %s\n", size, source, receivedData, receivedDatas)
 
 		// Create an empty response
 		response := []byte{}
